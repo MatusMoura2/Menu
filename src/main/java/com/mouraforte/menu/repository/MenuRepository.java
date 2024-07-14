5cf6471558741fd2fd811d3acf0f91058af52d33
Matus Moura <matus.moura333x2@gmail.com>
2024-07-14 15:03:35 -0300

    criação da interface de repositorio JPA

--- src/main/java/com/mouraforte/menu/repository/MenuRepository.java
+++ src/main/java/com/mouraforte/menu/repository/MenuRepository.java
@@ -1,5 +1,15 @@
 package com.mouraforte.menu.repository;
 
-public interface MenuRepository{
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mouraforte.menu.entity.Menu;
+
+public interface MenuRepository extends JpaRepository<Menu, UUID>{
+	List<Menu> findBySpecial(boolean special);
+	List<Menu> findByDayWeek(String dayWeek);
+	List<Menu> findBySnack(String snack);
 
 }
