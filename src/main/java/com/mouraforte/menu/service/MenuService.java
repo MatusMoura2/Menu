5330da75ae221cef1d8808430e2eb65e2fbc3c6c
Matus Moura <matus.moura333x2@gmail.com>
2024-07-14 16:15:05 -0300

    criando class de serviço com implementação de interfase service

--- src/main/java/com/mouraforte/menu/service/MenuService.java
+++ src/main/java/com/mouraforte/menu/service/MenuService.java
@@ -1,5 +1,18 @@
 package com.mouraforte.menu.service;
 
-public class MenuService {
+import java.util.List;
+import java.util.UUID;
 
+import com.mouraforte.menu.entity.Menu;
+
+public interface MenuService {
+
+	List<Menu> getAllMenus();
+	Menu getMenuId(UUID id);
+	Menu createMenu(Menu menu);
+	Menu updateMenu(UUID id, Menu menu);
+	void deleteMenu(UUID id);
+	List<Menu> getSpecialMenu();
+	List<Menu> getMenusByDayWeek(String dayWeek);
+	List<Menu> getMenusBySnack(String snack);
 }
--- src/main/java/com/mouraforte/menu/service/MenuServiceImpl.java
+++ src/main/java/com/mouraforte/menu/service/MenuServiceImpl.java
@@ -1,5 +1,61 @@
 package com.mouraforte.menu.service;
 
-public class MenuServiceImpl {
+import java.util.List;
+import java.util.UUID;
+
+import com.mouraforte.menu.entity.Menu;
+import com.mouraforte.menu.repository.MenuRepository;
+
+public class MenuServiceImpl implements MenuService{
+	
+	private MenuRepository repository;
+
+	@Override
+	public List<Menu> getAllMenus() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Menu getMenuId(UUID id) {
+		return repository.findById(id).orElseThrow(()-> new RuntimeException("menu not found!" + id));
+	}
+
+	@Override
+	public Menu createMenu(Menu menu) {
+		return repository.save(menu);
+	}
+
+	@Override
+	public Menu updateMenu(UUID id, Menu menu) {
+		Menu existingMenu = repository.findById(id).orElseThrow(()-> new RuntimeException("menu not found!" + id));
+		existingMenu.setName(menu.getName());
+		existingMenu.setDescription(menu.getDescription());
+		existingMenu.setPrice(menu.getPrice());
+		existingMenu.setSpecial(menu.isSpecial());
+		existingMenu.setDayWeek(menu.getDayWeek());
+		existingMenu.setSnack(menu.getSnack());
+		return repository.save(existingMenu);
+	}
+
+	@Override
+	public void deleteMenu(UUID id) {
+		Menu existingMenu = repository.findById(id).orElseThrow(()-> new RuntimeException("menu not found!" + id));
+		repository.delete(existingMenu);
+	}
+
+	@Override
+	public List<Menu> getSpecialMenu() {
+		return repository.findBySpecial(true);
+	}
+
+	@Override
+	public List<Menu> getMenusByDayWeek(String dayWeek) {
+		return repository.findByDayWeek(dayWeek);
+	}
+
+	@Override
+	public List<Menu> getMenusBySnack(String snack) {
+		return repository.findBySnack(snack);
+	}
 
 }
